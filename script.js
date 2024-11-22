2137b844e5689d9e1085c045beb1bd98172d2fe6
crlyflacko <peppociccone13@gmail.com>
2024-11-22 17:43:54 +0100

    Update: move grid maker into dedicate function
    Add function to remove grid

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+function createGrid(size){
+    for(let i = 0; i < size; i++){
+        let div = document.createElement("div");
+        container.appendChild(div);
+        // Adds event listeners to div and remove it when not necessary anymore
+        const changeBackground = () => {
+            div.style.backgroundColor = "blue";
+            div.removeEventListener("mouseenter", changeBackground);
+        }
+        div.addEventListener("mouseenter", changeBackground);
+    }
+}
+function removeGrid(){
+    let grid = document.querySelectorAll(".container > div");
+    for(div of grid){
+        container.removeChild(div);
+    }
+}
 // create a 16x16 grid of square divs and append it div.container
 let container = document.querySelector(".container");
-for(let i = 0; i < 256; i++){
-    let div = document.createElement("div");
-    container.appendChild(div);
-    // Adds event listeners to div and remove it when not necessary anymore
-    const changeBackground = () => {
-        div.style.backgroundColor = "blue";
-        div.removeEventListener("mouseenter", changeBackground);
-    }
-    div.addEventListener("mouseenter", changeBackground);
-    
-}
+squaresPerSide = 256;
+createGrid(squaresPerSide);
